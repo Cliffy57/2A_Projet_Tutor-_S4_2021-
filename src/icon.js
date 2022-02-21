b036afb8451dfd499d314f13a071014e0c87aeb2
Koshirei <76258309+Koshirei@users.noreply.github.com>
2022-02-21 18:24:30 +0100

    map-bdd
    markers bleu s'affichent sur la carte , coordonnées dans la bdd

---
+++ src/icon.js
@@ -0,0 +1,3 @@
+import L from 'leaflet';
+var iconrouge = L.icon({iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',iconSize:[25,41],iconAnchor:[12,41],popupAnchor:[1,-34]});
+export {iconrouge}
